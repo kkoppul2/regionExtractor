967963031632650841326958b5e9d60e719b7aaa
Jamshed Kaikaus <kaikaus2@illinois.edu>
2015-07-09 11:07:37 -0500

    using moments() instead of manual method to calc means

--- regionExtractor.cpp
+++ regionExtractor.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 int main(int argc, char *argv[])
 {
     Mat img = imread("/home/jkaikaus/Lab/regionExtractor/Samples/2.jpg");
-    Mat box, fin, temp;
+    Mat box, fin;
     cvtColor(img, box, COLOR_BGR2GRAY);
     Size s = img.size(); //obtain image dimensions
 	int rows = s.height;
@@ -20,8 +20,9 @@ int main(int argc, char *argv[])
     RNG rng(12345);
     MSER ms(5, 60, 14400, .25, .2, 200, 1.01, .003, 5);
     std::vector<vector<Point>> regions, contours, onehalf, two, three;
-    vector<Vec4i> hierarchy;
+    std::vector<Vec4i> hierarchy;
     ms(box, regions, Mat());
+    
     Point org;
     org.x = s.width/3+90;
     org.y = 50;
@@ -29,27 +30,28 @@ int main(int argc, char *argv[])
    	{
         drawContours(mask,regions,i,Scalar::all(255),1,8, vector<Vec4i>(), 0, Point());
         findContours(mask, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-        vector<vector<Point>> hull(contours.size());
+        std::vector<std::vector<Point>> hull(contours.size());
+        std::vector<Moments> mu(contours.size());
+        std::vector<Point2f> mc(contours.size());
         for (size_t j = 0; j < contours.size(); j++)
    	    {
+            mu[j]= moments(contours[j], false);
             convexHull(Mat(contours[j]), hull[j], false);
    	    }
-        std::vector<int>::iterator z = hull.begin();
-        while(!hull.end())
+        for(size_t a = 0; a < contours.size(); a++)
         {
-            onehalf = 1.5*(hull[z] - mean(hull));
-            two = 2*(hull[z] - mean(hull));
-            three = 3*(hull[z] - mean(hull));
+            mc[a] = Point2f( mu[a].m10/mu[a].m00 , mu[a].m01/mu[a].m00 );
+            
         }
+        for(
         for (size_t k = 0; k < contours.size(); k++)
         {
             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
             drawContours(img,contours,k,color,2,8, hierarchy, 0, Point());
-            drawContours(img,hull,k, Scalar(0,255,255), 2,8,vector<Vec4i>(), 0);
+            //drawContours(img,hull,k, Scalar(0,255,255), 2,8,vector<Vec4i>(), 0);
+            circle( img, mc[k], 4, Scalar::all(255), -1, 8, 0 );
         }
    	}
-    // sprintf(str, "%d", tags[i]);
-    //putText(img, "Standard Parameters\n MSER ms(10, 60, 14400, .25, .2, 200, 1.01, .003, 5)", org, FONT_HERSHEY_PLAIN, 1.5,  Scalar::all(0));
     imshow("/home/jkaikaus/Lab/regionExtractor/output/Samples/4.jpg", img);
     waitKey(0);
    	return 0;
